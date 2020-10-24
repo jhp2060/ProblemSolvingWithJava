55c2b1530d7b944ab57bea256bbf80654e808532
jhp2060 <jhp2060@gmail.com>
2020-10-24 13:33:25 +0900

    refactor(boj): queue 자료구조 설명 수정

--- BOJ/_DataStructureInJava.java
+++ BOJ/_DataStructureInJava.java
@@ -6,18 +6,19 @@ public class _DataStructureInJava {
         LinkedList<Integer> q = new LinkedList<>();
         int n;
 
-        // 값이 작은 인덱스로 삽입
+        // 인덱스 0으로 삽입
         q.push(1);
         q.push(2);
         q.push(3);
 
-        // 값이 높은 인덱스로 삽입
+        // 인덱스 size - 1 로 삽입
         q.add(-1);
         q.add(-2);
         q.add(-3);
 
         //n = q.pop(); // 인덱스 0 부터 pop
         //n = q.poll(); // 인덱스 0 부터 poll
+        //n = q.pollLast() // 인덱스 size - 1부터 poll
         n = q.remove(); // 인덱스 0 부터 remove
         System.out.println("end");
     }
