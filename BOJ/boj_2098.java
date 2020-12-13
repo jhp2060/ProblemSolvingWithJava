be19a7b9bce3dd372272f70c614e7183415b9856
jhp2060 <jhp2060@gmail.com>
2020-12-13 13:10:53 +0900

    fix(boj): boj 2098 - ret 참조 방법 변경

--- BOJ/boj_2098.java
+++ BOJ/boj_2098.java
@@ -9,14 +9,14 @@ public class boj_2098 {
 
     private static int N;
     private static int visitedAll;
-    private static int W[][];
+    private static int[][] W;
 
     // dp[i][status] : 현재까지 도시들을 방문한 상태(status)에서 현재 도시 i를 방문할 경우의 최소 거리
-    private static int dp[][];
+    private static int[][] dp;
 
     private static final int START = 0;
     private static final int INF = 16 * 1000000 + 1;
-동
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -42,16 +42,15 @@ public class boj_2098 {
         // 현재 상태에서 현재 노드를 방문한 적이 있는 지 판단
         int ret = dp[now][visited];
         if (ret > 0) return ret;
-        else dp[now][visited] = INF;
+        else ret = INF;
 
         for(int i = 0; i < N; i++) {
-            // 도시 i를 방문한 적이 없고, 현재 위치에서 i로 갈 수 있다면 이
+            // 도시 i를 방문한 적이 없고, 현재 위치에서 i로 갈 수 있다면 이동
             if((visited & (1<<i)) == 0 && W[now][i] > 0) {
-                dp[now][visited] = Math.min(dp[now][visited],
-                        TSP(i, visited|(1<<i)) + W[now][i]);
+                ret = Math.min(ret, TSP(i, visited|(1<<i)) + W[now][i]);
             }
         }
-        return dp[now][visited];
+        return dp[now][visited] = ret;
 
     }
     private static int stoi(String s) {return Integer.parseInt(s);}
